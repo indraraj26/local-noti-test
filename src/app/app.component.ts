049523f7fd45f576ac5a3047367c2ed487998a34
Indraraj Yadav <indra26@Indrarajs-Mac-mini.local>
2023-10-24 12:48:22 +0530

    feat: add local notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LocalNotifications } from '@awesome-cordova-plugins/local-notifications/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  clickSub: any;
+
+  constructor(
+    private localNotifications: LocalNotifications,
+    public alertController: AlertController
+  ) {}
+
+  ngOnInit() {
+    this.clickSub = this.localNotifications.on('click').subscribe((data) => {
+      this.presentAlert(
+        'Your notifiations contains a secret = ' + data.data.secret
+      );
+    });
+  }
+
+  async simpleNotif() {
+    try {
+      const isPermissionGiven = await this.localNotifications.hasPermission();
+      if (!isPermissionGiven) {
+        const requestPermissionResult =
+          await this.localNotifications.requestPermission();
+        if (!requestPermissionResult) {
+          throw new Error(
+            'please give permission, redirect the user to setting to enable it manually'
+          );
+        }
+      }
+      this.localNotifications.schedule({
+        id: 1,
+        text: 'Single Local Notification',
+        data: { secret: 'secret' },
+      });
+    } catch (e) {
+      console.log(e, 'error');
+    }
+  }
+
+  async presentAlert(data: any) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message: data,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { LocalNotifications } from '@awesome-cordova-plugins/local-notifications/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot()],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    LocalNotifications,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
